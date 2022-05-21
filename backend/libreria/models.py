857ee501dbd2475f904c964799a8f61f9e6354dc
maxexee <ruizmmaxm@gmail.com>
2022-05-20 22:34:55 -0500

    Finales 2, sin tiempo por irresponsables

--- backend/libreria/models.py
+++ backend/libreria/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class   servicio(models.Model):
+    id  =   models.AutoField(primary_key=True)
+    titulo  =   models.AutoField(max_length=100,    verbose_name='Titulo')
+    descripcion =   models.TextField(verbose_name='Descripción',    null=True)
