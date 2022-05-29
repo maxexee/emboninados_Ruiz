a609dd2b492e8ff995d6f57f0a945c002112e71d
maxexee <ruizmmaxm@gmail.com>
2022-05-28 22:10:05 -0500

    FInal ahora si- volver a formular

--- backend/libreria/urls.py
+++ backend/libreria/urls.py
@@ -13,7 +13,7 @@ urlpatterns  =   [
     path('servicios',   views.servicios,    name='servicios'),
     path('servicios/crear',   views.crearS,   name='crear'),
     path('borrarS/<int:id>',    views.borrarS, name='borrar'),
-    path('servicios/editarS/<int:id>',   views.editarS,  name='editar'),
+    path('servicios/editarS/<int:id>',   views.editarS,  name='editar'),    
 
     #PRODUCTOS...
     path('productos',   views.productos,    name='productos'),
